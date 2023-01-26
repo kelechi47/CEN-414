7f3960e62ccf398f90aa35d5cd07784b01ab3b35
kelechi47 <117107033+kelechi47@users.noreply.github.com>
2023-01-26 12:17:20 +0100

    Add files via upload

---
+++ cen414.java
@@ -0,0 +1,93 @@
+public class cen414{
+  
+  public static void main(String[] args) {
+    System.out.println("19CJ025817");
+    
+    int x,y;
+    x=1;
+    y=7;
+    System.out.println(Math.sqrt(17));
+     System.out.println(Math.abs(17));
+      System.out.println(Math.cbrt(17));
+       System.out.println(Math.exp(17));
+        System.out.println(Math.pow(1,7));
+        
+        
+if(1>7){
+          System.out.println("1 is greater than 7");
+}
+else
+          {
+        System.out.println("7 is greater than 1");  
+  }
+
+
+String statement_1,statement_2;
+  statement_1="Hello";
+  statement_2="Hi";
+   String name;
+  name = x>y ? statement_1:statement_2;
+   System.out.println(statement_1);  
+
+   
+ int day = 6;
+  switch (day){
+   case 1 : 
+  System.out.println("monday");   
+  break;
+  
+   case 2 : 
+  System.out.println("tuesday");   
+  break;
+  
+   case 3 : 
+  System.out.println("wednesday");   
+  break;
+ 
+   case 4 : 
+  System.out.println("thursday");   
+  break;
+ 
+   case 5 : 
+  System.out.println("friday");   
+  break;
+  
+   case 6 : 
+  System.out.println("saturday");   
+  break;
+  
+   case 7 : 
+  System.out.println("sunday");   
+  break;
+  
+    default:
+      System.out.println("not a day");
+      break;
+   }
+
+
+
+
+
+
+System.out.println(x<y && x>y);
+System.out.println(x<y || x>y);
+int and = x&y;
+int or = x|y;
+int xor = x^y;
+boolean not = !(x>y);
+System.out.println(not);
+System.out.println(and);
+System.out.println(or);
+System.out.println(xor);
+  
+ 
+
+for(int i=0;i<=99;i++){
+  if(i==17)
+  { System.out.println(i);
+  }
+}
+}
+}
+  
